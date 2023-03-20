7f6007e559966d66cbd2bfbeab0d22bdf708260d
장일규 <jang19.dev@gmail.com>
2023-03-20 23:28:26 +0900

    feat: 배치 로직 수정

--- utils/fetchCitiesData.js
+++ utils/fetchCitiesData.js
@@ -3,53 +3,86 @@ const xml2js = require('xml2js');
 const parser = new xml2js.Parser({explicitArray:false, mergeAttrs : true});
 const fetch = require('node-fetch');
 const City = require('../models/City');
-const cityArr = require('./cityArr');
-let index = require('../utils/index');
+ const {OPEN_API_SEOUL, API_KEY, TYPE, SERVICE, START_INDEX, END_INDEX} = process.env;
 
 const fetchCitiesData = async(city) => {
 
-    var uri = `${process.env.OPEN_API_SEOUL}/${process.env.API_KEY}/${process.env.TYPE}/${process.env.SERVICE}/${process.env.START_INDEX}/${process.env.END_INDEX}/${city}`;
-    var res1 = encodeURI(uri);
-    console.log(res1);
-    await fetch(res1)
-    .then((res) => res.text())
-    .then((xml) => { 
-        parser.parseStringPromise(xml).then(function (result) {
-            const AREA_NM = result["SeoulRtd.citydata"].CITYDATA.AREA_NM;
-            const LIVE_PPLTN_STTS = result["SeoulRtd.citydata"].CITYDATA.LIVE_PPLTN_STTS.LIVE_PPLTN_STTS;
-            const ROAD_TRAFFIC_STTS = result["SeoulRtd.citydata"].CITYDATA.ROAD_TRAFFIC_STTS.AVG_ROAD_DATA;
-            const SBIKE_STTS = result["SeoulRtd.citydata"].CITYDATA.SBIKE_STTS.SBIKE_STTS;
-            
-            const city = {};
-            city.AREA_NM = AREA_NM;
-            city.LIVE_PPLTN_STTS = LIVE_PPLTN_STTS
-            city.AVG_ROAD_DATA = ROAD_TRAFFIC_STTS;
-            if (SBIKE_STTS !== "") {
-              city.SBIKE_STTS = SBIKE_STTS;
-            }
+    var uri = `${OPEN_API_SEOUL}/${API_KEY}/${TYPE}/${SERVICE}/${START_INDEX}/${END_INDEX}/${city}`;
+    var encodedURI = encodeURI(uri);
+    var response = await fetch(encodedURI);
+    var xml = await response.text();
+    var res = await parser.parseStringPromise(xml);
 
-            ++index.sharedInt;
-            cityArr.push(city);
-        });
-    })
-    .then(()=>{
-        if(index.sharedInt === 50) {
-          City.findOneAndDelete({ 'cities' : { '$exists': true} }, (err, doc) => {
-            if (err) {
-                  console.log(err);
-                } else {
-                  let newDoc = new City({ cities: cityArr });
-                  newDoc.save((err, newDoc) => {
-                    if (err) {
-                      console.log(err);
-                    } else {
-                      console.log("Deleted and re-created document:", newDoc);
-                    }
-                  });
-                }
-              });
+    const { AREA_NM, LIVE_PPLTN_STTS, ROAD_TRAFFIC_STTS, SBIKE_STTS } = res["SeoulRtd.citydata"].CITYDATA;
+    const { ROAD_MSG, ROAD_TRAFFIC_IDX, ROAD_TRFFIC_TIME, ROAD_TRAFFIC_SPD } = ROAD_TRAFFIC_STTS.AVG_ROAD_DATA;
+    const sBikeStats = SBIKE_STTS ? SBIKE_STTS.SBIKE_STTS : [];
+    const sBikeSpotNames = sBikeStats;
+
+    const { 
+        LIVE_PPLTN_STTS: { 
+            AREA_CONGEST_LVL, 
+            AREA_CONGEST_MSG, 
+            AREA_PPLTN_MIN, 
+            AREA_PPLTN_MAX, 
+            MALE_PPLTN_RATE, 
+            FEMALE_PPLTN_RATE, 
+            PPLTN_RATE_0, 
+            PPLTN_RATE_10, 
+            PPLTN_RATE_20, 
+            PPLTN_RATE_30, 
+            PPLTN_RATE_40, 
+            PPLTN_RATE_50, 
+            PPLTN_RATE_60, 
+            PPLTN_RATE_70, 
+            RESNT_PPLTN_RATE, 
+            NON_RESNT_PPLTN_RATE, 
+            REPLACE_YN, 
+            PPLTN_TIME 
+        } 
+    } = LIVE_PPLTN_STTS;
+    
+    const cityObj = { 
+        AREA_NM, 
+        LIVE_PPLTN_STTS: { 
+            AREA_CONGEST_LVL, 
+            AREA_CONGEST_MSG, 
+            AREA_PPLTN_MIN, 
+            AREA_PPLTN_MAX, 
+            MALE_PPLTN_RATE, 
+            FEMALE_PPLTN_RATE, 
+            PPLTN_RATE_0, 
+            PPLTN_RATE_10, 
+            PPLTN_RATE_20, 
+            PPLTN_RATE_30, 
+            PPLTN_RATE_40, 
+            PPLTN_RATE_50, 
+            PPLTN_RATE_60, 
+            PPLTN_RATE_70, 
+            RESNT_PPLTN_RATE, 
+            NON_RESNT_PPLTN_RATE, 
+            REPLACE_YN, 
+            PPLTN_TIME 
+        }, 
+        AVG_ROAD_DATA: { 
+            ROAD_MSG, 
+            ROAD_TRAFFIC_IDX, 
+            ROAD_TRFFIC_TIME, 
+            ROAD_TRAFFIC_SPD 
+        },
+        SBIKE_STTS: sBikeSpotNames,
+    };
+    
+    City.updateOne(
+      { "cities.AREA_NM": cityObj.AREA_NM },
+      { $set: { "cities.$": cityObj } },
+      (err, result) => {
+        if (err) {
+          console.log("Error: ", err);
+        } else {
+            console.log("Error: ", result);
         }
-    });
+      }
+    );
 }
 
 module.exports = fetchCitiesData
